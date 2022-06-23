9e770b9b879896c35d95d2a47ec2c28ab84a8392
IBUKUN OLODUDE <104784071+Daveduke9@users.noreply.github.com>
2022-06-23 21:03:32 +0100

    Zuri Django Models Task

--- I4G033444UME/I4G033444UME/settings.py
+++ I4G033444UME/I4G033444UME/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from ctypes import cast
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-63bc2d25h)1@$c71!a(aufmhqeu)ds$&x&2-ajdsh(yzaa3^1v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
